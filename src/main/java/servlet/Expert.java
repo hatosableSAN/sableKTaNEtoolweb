7e8a334353fba1d75f893ea019a2275008a2c7c5
Hatosable <hatopyonkuruppo1169@gmail.com>
2022-01-28 12:07:34 +0900

    modify lint tool

--- src/main/java/servlet/Expert.java
+++ src/main/java/servlet/Expert.java
@@ -1,8 +1,6 @@
 package servlet;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 @WebServlet("/Expert")
 @MultipartConfig
