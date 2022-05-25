aecc59855057f2767d876c193a3ca526bc084de6
hatosable <hatopyonkuruppo1169@gmail.com>
2022-05-25 14:48:42 +0900

    エラー出る問題修正

--- discordbot.py
+++ discordbot.py
@@ -17,17 +17,21 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def start(ctx,arg):
-     LobbyName = arg
-     Guild = ctx.guild
+async def makefield(ctx,day,name):
+     if not day or not name:
+      await ctx.send("チャンネル名が空ですよ！")
+     else:
 
-     # カテゴリを作成する
-     Category = await Guild.create_category(LobbyName)
+       LobbyName = day+" "+name
+       Guild = ctx.guild
 
-     # チャンネルの作成時にカテゴリを設定する
-     await Category.create_text_channel("女神降臨場")
-     await Category.create_text_channel("メニュー宣言ゾーン")
-     await ctx.send("カテゴリーを作成しました！")
+       # カテゴリを作成する
+       Category = await Guild.create_category(LobbyName)
+
+       # チャンネルの作成時にカテゴリを設定する
+       await Category.create_text_channel("女神降臨場")
+       await Category.create_text_channel("メニュー宣言ゾーン")
+       await ctx.send("カテゴリーを作成しました！")
 
 
 
