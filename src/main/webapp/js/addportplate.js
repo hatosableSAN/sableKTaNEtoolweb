7cbfe24df7f2566561f39a9fbf082b4c59b6dcde
hatosable <hatopyonkuruppo1169@gmail.com>
2022-06-18 20:55:11 +0900

    ポートプレートさらに修正

--- src/main/webapp/js/addportplate.js
+++ src/main/webapp/js/addportplate.js
@@ -1,18 +1,30 @@
+clone=0;
+  $(document).on("click", ".add", function() {
+  clone=clone+1;
+  var clonename="clone"+String(clone);
+  var cloneport= $(this).parents(".original").clone(true).removeClass('original').addClass(clonename);
+  var originalport=$(this).parents("#portplate-contents").addClass('original');
+  if(clone!=1){
+   var parentname=".clone"+String(clone-1);
+   originalport=$(parentname);
+   console.log(parentname);
+   console.log(originalport);
+  }
+  cloneport.insertAfter(originalport);//ポートボックスの複製 末尾に追加されてくれ
+  cloneport.find('input[name="port"]').prop('checked',false);//チェックボックスリセット
+  cloneport.children("#port-button").remove();//クローンのボタン削除
 
-$(document).on("click", ".add", function() {
-
-  const cloneport= $(this).parents(".original").clone(false).removeClass('original').addClass('clone');
-  const originalport=$(this).parents("#portplate-contents").addClass('original');
-  cloneport.insertAfter(originalport);
-  //ポートボックスの複製
-  $(this).parent().next().find('input[name="port"]').prop('checked',false);
-  $('.clone').children("#port-button").remove();
+  
 });
     $(document).on("click", ".del", function() {
-        var target = $(this).parent();
-        if (target.parent().children().length > 1) {
+     var targetname=".clone"+String(clone);
+     console.log(targetname);
+        var target = $(targetname);
+
             target.remove();
-        }
+     if(clone>0){
+      clone=clone-1;
+     }
       
     });
 
