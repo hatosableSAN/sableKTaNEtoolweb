91341be89c12aa66bb1f4984251e0cadc5ab60f8
hatosable <hatopyonkuruppo1169@gmail.com>
2022-06-24 17:05:00 +0900

    track1を出す

--- discordbot.py
+++ discordbot.py
@@ -447,7 +447,7 @@ async def GdbGSelect(ctx):
    albumlist=[[7,7,6,6,6],[17,17,5],[12,12,11],[12,12,13],[12,12,12],[10,9,9,10],[10,10,10,11],[10,10,10,10],[20,20],[6,5,5,6,5,5,5,5],[10,9,10,10,10],[12,12,12,12],[12,12,12,12]]
    thisalbum=albumlist[albumnum]
    albumsize=len(thisalbum)
-   discpos=random.randint(1,albumsize)#ディスクきめ
+   discpos=random.randint(1,albumsize)-1#ディスクきめ
    disc=thisalbum[discpos]
    track=random.randint(1,disc)
 
