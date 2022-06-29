3f2300ba5157502736ada841b1695075d970b446
hatosable <hatopyonkuruppo1169@gmail.com>
2022-06-29 09:43:57 +0900

    naositayo

--- discordbot.py
+++ discordbot.py
@@ -447,8 +447,8 @@ async def GdbGSelect(ctx):
    albumlist=[[7,7,6,6,6],[17,17,5],[12,12,11],[12,12,13],[12,12,12],[10,9,9,10],[10,10,10,11],[10,10,10,10],[20,20],[6,5,5,6,5,5,5,5],[10,9,10,10,10],[12,12,12,12],[12,12,12,12]]
    thisalbum=albumlist[albumnum]
    albumsize=len(thisalbum)
-   discpos=random.randint(1,albumsize)-1#ディスクきめ
-   disc=thisalbum[discpos]
+   discpos=random.randint(1,albumsize)#ディスクきめ
+   disc=thisalbum[discpos-1]
    track=random.randint(1,disc)
 
    String=URLCommonStr+str(year)+"-"+str(discpos)+"-"+str(track)
