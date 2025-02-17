443a192c71bbcb74a15150723d9ef1d834c4383b
hatosable <hatopyonkuruppo1169@gmail.com>
2025-02-18 00:14:55 +0900

    huruinoni taiou

--- src/main/java/servlet/Converter.java
+++ src/main/java/servlet/Converter.java
@@ -81,7 +81,7 @@ while ((text = br.readLine()) != null) {
 if(text.contains("<meta property=\"")){//メタデータは除外
   	continue;
   }
-if(text.contains("<html lang=\"en\">")){
+if(text.contains("<html lang=\"en\">") || text.contains("<html>")){
   	text="<html lang=\"ja\">";//ja入りに置き換え
   	write(writer,text);
   }
